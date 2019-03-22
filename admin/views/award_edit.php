a059233a37607031b9747c991305001e8ac9467b
Spraynard <kellan.martin@gmail.com>
2019-03-22 14:40:50 -0400

    committing changes before rewrite

--- admin/views/award_edit.php
+++ admin/views/award_edit.php
@@ -17,19 +17,19 @@
 		<input type="hidden" name="action" value="update_award"/>
 
 		<!-- Name Input -->
-		<label for="award-name">Award Title</label>
+		<label for="award-name"><h2>Award Title</h2></label><br/>
 		<input id="award-name" name="award_title" value="<?php echo $award->title; ?>" type="text"/><br/>
 
 		<!-- Description Input -->
-		<label for="award-description">Award Description</label>
+		<label for="award-description"><h2>Award Description</h2></label><br/>
 		<textarea id="award-description" name="award_description"><?php echo $award->description; ?></textarea><br/>
 
 		<!-- Date Created -->
-		<label for="award-date-created"></label>
+		<label for="award-date-created"><h2>Date Created</h2></label><br/>
 		<input id="award-date-created" name="date_created" value="<?php echo $award->date_created; ?>" type="text" readonly /><br/>
 
 		<!-- Date Modified -->
-		<label for="award-date-modified"></label>
+		<label for="award-date-modified"><h2>Date Last Modified</h2></label><br/>
 		<input id="award-date-created" name="date_modified" value="<?php echo $award->date_modified; ?>" type="text" readonly /><br/>
 
 		<!-- Submit -->
