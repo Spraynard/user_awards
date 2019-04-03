88b7e7dc17f48aa72afb45b33bcf091c04e587f4
Spraynard <kellan.martin@gmail.com>
2019-04-03 02:40:31 -0400

    Updating tests

--- tests/test-wp-awards-wordpress.php
+++ tests/test-wp-awards-wordpress.php
@@ -12,50 +12,31 @@
  * is supported.
  */
 class WPAwardsTest extends WP_UnitTestCase {
+	// Post type to test against.
+	private $post;
 
 	// Set up award posts before we run any tests.
 	public function setUp() {
-		$postarrfiftyhours = array(
+		parent::setUp();
+
+		// Testing that we have our custom post type
+		$this->assertTrue(post_type_exists('wap_award'));
+
+		// Assigning to our post variable.
+		$this->post = $this->factory->post->create_and_get(array(
 			'post_type' => 'wap_award',
 			'post_title' => 'Fifty Hours Worked',
 			'post_content' => 'Awarded to users if they have more than 50 hours worked for us. They are really nice people',
-			'post_author' => 'Test Admin',
+			'post_author' => 1,
 			'meta_input' => array(
 				'wap_grammar' => "CURRENT_USER_META UPDATED WHERE key=total_hours GTEQ 50"
 			)
-		);
-
-		$setupArray = array(
-			$postarrfiftyhours
-		);
-
-		foreach( $setupArray as $postArray )
-		{
-			echo "wp_insert_post here:\n";
-			print_r( $postArray );
-			$post_id = wp_insert_post( $postArray );
-
-			if ( $post_id )
-			{
-				echo "We have successfully created a post with POST_ID: {$post_id}";
-			}
-			else
-			{
-				echo "There was a problem creating a post";
-			}
-		}
+		));
 	}
 
-
 	public function testNumberOne() {
-		$post_type_args = array(
-			'post_type' => 'wap_award'
-		);
 
-		$wap_awards_posts = get_posts();
-
-		echo "These are our posts\n";
-		print_r( $wap_awards_posts );
+		print_r( $this->post );
 		$this->assertTrue(true);
 	}
 }
