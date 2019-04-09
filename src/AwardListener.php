328208832e8e234ed2a9dc117e4822831c2935ac
Spraynard <kellan.martin@gmail.com>
2019-04-09 02:13:40 -0400

    AwardListener development to listen to events and apply awards to users based on events.

--- src/AwardListener.php
+++ src/AwardListener.php
@@ -3,53 +3,73 @@ namespace WPAward;
 
 class AwardListener {
 	private $grammar = null;
-
+	private $grammarFunction = null;
 	function __construct( $grammar_string ) {
 		$this->grammar = new AwardGrammar( $grammar_string );
+		$this->grammarFunction = $this->grammar->entity . '_' . $this->grammar->trigger_type;
 	}
 
 	public function add_listeners( $user ) {
 
+		$action = "";
+
 		if ( ! is_a( $user, 'WP_User') ) {
-			return false;
+			throw new \InvalidArgumentException("User is not a WP_User");
 		};
 
 		// If we don't have a current user, there's no reason to give someone awards
-		if ( ! $current_user->exists() )
+		if ( ! $user->exists() )
 		{
-			return false;
+			throw new \UnexpectedValueException("User does not exist...");
 		}
 
+		// Pre-set our trigger tye based on whether the user has a user meta field or not.
+		if ( $this->grammar->trigger_type == "assigned" )
+		{
+			if ( empty( get_user_meta($user->ID, $this->grammar->trigger->descriptor->key) ) )
+			{
+				$this->grammar->change_trigger_type("added_user_meta");
+			}
+			else
+			{
+				$this->grammar->change_trigger_type("updated_user_meta");
+			}
+		}
+
+		// Explicit update
 		if ( $this->grammar->trigger_type == "updated" )
 		{
-			do_action('updated_user_meta', [
-				$this,
-				call_user_func(
-					$this->grammar->entity . '_' . $this->grammar->trigger_type,
-					$current_user
-				)
-				// example: current_user_meta_updated
-
-			], 10, 4);
+			$action = "updated_user_meta";
 		}
+		// Explicit Create
+		else if ( $this->grammar->trigger_type == "created" )
+		{
+			$action = "added_user_meta";
+		}
+
+		add_action($action, [ $this, $this->grammarFunction ], 10, 4); // example: current_user_meta_updated
 	}
 
 	/**
 	 * Function that responds to a user metadata update.
 	 * Here we will test our grammar conditions
 	 */
-	function current_user_meta_updated( $user ) {
+	function current_user_meta_updated( $meta_id, $object_id, $meta_key, $_meta_value ) {
+		echo "Current User Meta Getting Updated";
+		echo "VALUES:\n$meta_id\n, $object_id\n, $meta_key\n, $_meta_value\n";
 		// Award user if our current user's meta key passes the grammar control
-		$user_meta = get_user_meta( $user );
-		$trigger_descriptor = $this->grammar->trigger_descriptor->key;
+		$user_meta = get_user_meta( $object_id );
+		$descriptor = $this->grammar->trigger->descriptor->key;
 
 		// Check to see if our entity has a trigger descriptor associated with it.
-		if ( ! is_array($user_meta) || empty( $user_meta[$trigger_descriptor] ) )
+		if ( ! is_array($user_meta) || empty( $user_meta[$descriptor] ) )
 		{
 			return;
 		}
 
-		if ( )
+		echo "User Meta";
+		print_r( $user_meta );
+		// if ( )
 	}
 
 	function current_user_meta_excluded() {
