7f6019a91e37a5173dfeba59d4c6c65c7a154fdd
Spraynard <kellan.martin@gmail.com>
2019-04-30 20:25:45 -0400

    Updating UpdatePostmetaAction
    Updating constructor as well as adding in the ability to throw an exception if we do not have the required value before we try and output data

--- src/Actions/UpdatePostMetaAction.php
+++ src/Actions/UpdatePostMetaAction.php
@@ -5,12 +5,17 @@ namespace WPAward\Actions;
 class UpdatePostMetaAction extends PostBasedAction {
 	private $source;
 
-	function __construct( $name, $post_id, $source = NULL ) {
+	function __construct( $name, $post_id = NULL, $source = NULL ) {
 		parent::__construct( $name, $post_id );
 		$this->source = ( empty( $source ) ) ? $_POST : $source;
 	}
 
 	function main_execute() {
+		if ( is_null( $this->post_id ) )
+		{
+			throw new Exception("Post ID is NULL");
+		}
+
 		update_post_meta( $this->post_id, $this->name, $source[$this->name] );
 	}
 }
