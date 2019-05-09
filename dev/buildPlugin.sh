1448efe46c27f2ae44cb902d4d989c031d38517f
Spraynard <kellan.martin@gmail.com>
2019-05-09 16:35:21 -0400

    Adding to gitignore and coming up with another development script
    .gitignore blocks dist/ directory
    buildPlugin is a file that creates a distributable zip file for our plugin.

---
+++ dev/buildPlugin.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This file is used to build our plugin for acceptable use in the WordPress
+# plugin directory. We zip up the following items into dist/
+#	* src/
+#	* assets/
+#	* vendor/
+#	* constants.php
+#	* wp_awards.php
+#	* LICENSE.txt
+#	* readme.txt
+
+# Build dist if not available
+
+if [ ! -d dist ]
+then
+	echo "dist/ not available. Creating this..."
+	mkdir dist
+fi
+
+
+# -r --- Recursive zipping?
+# -j --- Junk paths. Don't store our host directory structure.
+zip -r --exclude=*.DS_Store* dist/user_awards.zip src/ assets/ vendor/ constants.php wp_awards.php LICENSE.txt readme.txt
