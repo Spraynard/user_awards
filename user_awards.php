d4d29811a85290168fda1786bd45f345ec2ca70d
Spraynard <kellan.martin@gmail.com>
2019-05-19 19:13:13 -0400

    Changing description of plugin

--- user_awards.php
+++ user_awards.php
@@ -1,7 +1,7 @@
 <?php
 /**
 Plugin Name: User Awards
-Description: Let your registered members know how much you appreciate them! Enhances your site with the abilty to assign and give awards to members based on the actions that they take. Your members will have a lifetime supply of cherished memories that they can hold dear to their hearts for years to come after using your site!
+Description: Let your registered members know how much you appreciate them! Enhances your site with the abilty to assign and give awards to members based on the actions that they take. Your members will have a lifetime supply of cherished memories that they can hold dear to their hearts for years to come.
 Author: Kellan Martin
 Version: 0.0.1
 Author URI: http://kellanmartin.com
